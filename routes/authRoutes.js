2ebf36a5b2fd37c33b12123e56bc5021ea157752
Sam Bowen <samhbowen@gmail.com>
2025-03-12 18:45:11 -0700

    fix MongoDB_URI

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const verifyToken = require('../middleware/verify-token');
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const cors = require('cors');
 
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error(err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
@@ -25,5 +23,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
