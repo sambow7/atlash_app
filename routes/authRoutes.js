a6686a0ee1ff0e58259950925356c14a1dc16040
Sam Bowen <samhbowen@gmail.com>
2025-03-22 16:07:57 -0700

    user  to User

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models/User');
+const User = require('../models/User');
 const verifyToken = require('../middleware/verify-token');
 
 const router = express.Router();
